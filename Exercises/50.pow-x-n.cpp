1875c2f25dc821ac9dd2485db3151ef0a65ce907
Lyuyk <csap001@qq.com>
2023-04-06 20:53:11 +0800

    Create 50.pow-x-n.cpp

---
+++ Exercises/50.pow-x-n.cpp
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=50 lang=cpp
+ *
+ * [50] Pow(x, n)
+ */
+
+// @lc code=start
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long b = n;
+        double res = 1.0;
+        while(b){
+            if(b<0){
+                x=1/x;
+                b=-b;
+            }
+            //按位与判断
+            if(b&1==1){
+                res=res*x;
+            }
+            x=x*x;
+            b>>=1; //右移即乘2
+        }
+    }
+};
+// @lc code=end
+
